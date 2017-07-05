a50c770b7e37b5f0f14da8d173212020d5d34aa8
Ech0-jp <hit-the-hills@hotmail.ca>
2017-07-05 18:12:28 -0400

    initial commit.

---
+++ app/src/main/java/io/github/ech0_jp/sudoku/MainMenu.java
@@ -0,0 +1,13 @@
+package io.github.ech0_jp.sudoku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
